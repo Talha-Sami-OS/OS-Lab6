35e770c9cc35f4e6e706a4bc551aaa5557443a8c
Talha&Sami <70809552+Talha-Sami-OS@users.noreply.github.com>
2021-06-20 23:34:45 +0500

    Update translate.c

--- translate.c
+++ translate.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
-#include <unistd.h>
 #include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include<string.h>
+#include <string.h>
+
+const int pagetable_enteries = 256;
+const int page_size = 256;
+const int TLB_size = 16;
+const int frame_size = 256;
+
+void *thread(void*para)
+{
+	
+	int *para=*int (*para);
+	for(int i=0;i<4096;i++)
+	{
+		if(buffer[i]==a)
+		{
+			buffer[i]=b;
+		}
+	
+	}
+	pthread_exit(para);
+}
+
+int main(int agrc,char*arg[])
+{
+  long long page_no,offset,page_table,totalhits=0,fault=0,pages=0;
+  
+  int hit = 0;
+  
+  
+}
